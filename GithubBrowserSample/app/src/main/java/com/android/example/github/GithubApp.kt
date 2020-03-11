274ea5702cb5f10ea13012ce6d9fc6b6896f471e
Michael Evans <mevans@twitter.com>
2020-03-10 23:56:17 -0400

    Add dependency and configure flipper

--- GithubBrowserSample/app/src/main/java/com/android/example/github/GithubApp.kt
+++ GithubBrowserSample/app/src/main/java/com/android/example/github/GithubApp.kt
@@ -19,11 +19,16 @@ package com.android.example.github
 import android.app.Activity
 import android.app.Application
 import com.android.example.github.di.AppInjector
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import timber.log.Timber
 import javax.inject.Inject
-
 
 class GithubApp : Application(), HasActivityInjector {
     @Inject
@@ -31,6 +36,15 @@ class GithubApp : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+        SoLoader.init(this, false);
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this);
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()));
+            client.addPlugin(DatabasesFlipperPlugin(this));
+            client.start();
+        }
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
